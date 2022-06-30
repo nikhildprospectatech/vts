83e92b3e5e58677e265f025c37cec758a0fbbd18
Nikhil Desai <nikhild@prospectatech.local>
2022-06-30 17:07:39 +0530

    changes

--- src/app/components/vehicle-info/vehicle-info.component.ts
+++ src/app/components/vehicle-info/vehicle-info.component.ts
@@ -26,13 +26,14 @@ export class VehicleInfoComponent implements OnInit {
   ];
 
   page = 1;
-  pagelimit = 10;
+  pagelimit = 1000;
 
   limit = 5;
 
   inOutStatus :boolean = false;
   vehicleData;
   dataSource: MatTableDataSource<any>;
+  vehicleCount24hr = 0;
 
   displayedColumns: string[] = ['Sl.No.', 'Date', 'Vehicle Number', 'Category', 'From Hour', 'To Hour', 'Check In/out', "Duration"];
   constructor(
@@ -49,6 +50,7 @@ export class VehicleInfoComponent implements OnInit {
     this.vehicleData = res[0];
     this.dataSource = new MatTableDataSource(res[0].items);
     this.dataSource.paginator = this.paginator;
+    this.calc(res[0].items);
   }
 
   toggle(val, item){
@@ -60,6 +62,36 @@ export class VehicleInfoComponent implements OnInit {
     });
   }
 
+  calc(arr){
+    this.vehicleCount24hr = 0;
+    arr.forEach((el) => {
+     let hoursBetweenDates = this.isLessThan24HourAgo(new Date(el.date * 1000))
+     if(hoursBetweenDates){
+      this.vehicleCount24hr++
+     }
+    })
+
+    
+  }
+
+  isLessThan24HourAgo(date) {
+    // hour  min  sec  milliseconds
+    const twentyFourHrInMs = 24 * 60 * 60 * 1000;
+  
+    const twentyFourHoursAgo = Date.now() - twentyFourHrInMs;
+  
+    return date > twentyFourHoursAgo && date <= Date.now();
+  }
+
+
+  // durationCount(val){
+  //   console.log(val)
+
+  //   console.log(new Date(val.exitDate * 1000).toLocaleDateString())
+
+
+  //   return 
+  // }
 
   // timeLeft: number = 60;
   // interval;
