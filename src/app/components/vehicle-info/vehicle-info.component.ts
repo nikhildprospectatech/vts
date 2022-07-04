9b29ba6607a49ac2b1ba0a061bdc08496c331080
Nikhil Desai <nikhild@prospectatech.local>
2022-07-04 12:15:36 +0530

    changes

--- src/app/components/vehicle-info/vehicle-info.component.ts
+++ src/app/components/vehicle-info/vehicle-info.component.ts
@@ -90,7 +90,12 @@ export class VehicleInfoComponent implements OnInit {
     
     let diff = ( dt2.getTime() - dt1.getTime()) / 1000;
     diff /= (60 * 60)
-    return Math.abs(Math.round(diff))
+
+    let diffMin = dt2.getTime() - dt1.getTime();
+
+    let minutes = Math.floor(diffMin/ (1000 * 60) % 60)
+    
+    return Math.abs(Math.round(diff)) + ':' + minutes
   }
 }
 
